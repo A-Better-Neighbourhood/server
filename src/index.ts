9de8c27403a741f38473d0b86ce911ce1eaf5317
Kunal Rana <kunalranarj2005@gmail.com>
2025-09-19 23:31:23 +0530

    feat(prisma): added prisma

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 /** @format */
 import express from "express";
+import { env } from "./configs/env.js";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+env.parse(process.env);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
