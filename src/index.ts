9e6b5c9a5f17fc52128887f22a2aa02b9021d049
Kunal Rana <kunalranarj2005@gmail.com>
2025-12-11 18:02:39 +0530

    fixed run issues

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import { router } from "./routes/index.route";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
